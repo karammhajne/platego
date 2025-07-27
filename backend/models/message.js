177a64147e73725de915158771747554e7f8b3f3
karam mhajne <karammhajne@gmail.com>
2025-07-28 02:13:51 +0300

    real time sending pics in chats

--- backend/models/message.js
+++ backend/models/message.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
   chat: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true },
+  text: { type: String, default: "" },
   image: { type: String, default: null }, // ✅ added this
   timestamp: { type: Date, default: Date.now }
 });
