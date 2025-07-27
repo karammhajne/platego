775932bae575509bf24251d0590ad855151d7ed0
karam mhajne <karammhajne@gmail.com>
2025-07-28 01:57:45 +0300

    real time sending pics in chats

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors({
 }));
 
 app.use(bodyParser.json({ limit: "10mb" }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true limit: "10mb" }));
 
 app.use((req, res, next) => {
   req.io = io;
