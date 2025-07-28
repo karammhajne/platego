8fb6b383ee20ff41539295295718160cb5e0ae9a
karam mhajne <karammhajne@gmail.com>
2025-07-28 22:41:13 +0300

    Fix: added authMiddleware to users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
+
 
 
 router.post('/register', userController.registerUser);
